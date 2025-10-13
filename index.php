6825aa75bfd49194206373eb747043040ea8fd7e
Vyacheslav Antonov <vyacheslav.antonov.job@gmail.com>
2025-10-13 17:53:11 +0300

    Init commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Мебельная компания");
+?><p>
+	Наша компания существует на Российском рынке с 1992 года. За это время «Мебельная компания» прошла большой путь от маленькой торговой фирмы до одного из крупнейших производителей корпусной мебели в России.
+</p>
+<p>
+	 «Мебельная компания» осуществляет производство мебели на высококлассном оборудовании с применением минимальной доли ручного труда, что позволяет обеспечить высокое качество нашей продукции. Налажен производственный процесс как массового и индивидуального характера, что с одной стороны позволяет обеспечить постоянную номенклатуру изделий и индивидуальный подход – с другой.
+</p>
+<h3>Наша продукция</h3>
+ <?$APPLICATION->IncludeComponent(
+	"bitrix:furniture.catalog.index",
+	"",
+	Array(
+		"CACHE_GROUPS" => "N",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"IBLOCK_BINDING" => "section",
+		"IBLOCK_ID" => "2",
+		"IBLOCK_TYPE" => "products"
+	)
+);?>
+<h3>Наши услуги</h3>
+ <?$APPLICATION->IncludeComponent(
+	"bitrix:furniture.catalog.index",
+	"",
+	Array(
+		"CACHE_GROUPS" => "N",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"IBLOCK_BINDING" => "element",
+		"IBLOCK_ID" => "3",
+		"IBLOCK_TYPE" => "products"
+	)
+);?>
+<p>
+</p><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
