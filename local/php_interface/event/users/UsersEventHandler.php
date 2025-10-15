97be982409dd746db15f30dd671bfbd4f89faa3e
Vyacheslav Antonov <vyacheslav.antonov.job@gmail.com>
2025-10-15 15:12:26 +0300

    import added

--- local/php_interface/event/users/UsersEventHandler.php
+++ local/php_interface/event/users/UsersEventHandler.php
@@ -2,31 +2,36 @@
 
 namespace Event\Users;
 
+use Bitrix\Main\Loader;
+use CUser;
+use CIBlockElement;
+use Bitrix\Main\Mail\Event;
+
 class UsersEventHandler
 {
     private static $oldAuthorStatus;
 
     public static function onBeforeUserUpdateHandler(&$arFields)
     {
-        if (\Bitrix\Main\Loader::includeModule("iblock") && \Bitrix\Main\Loader::includeModule("main"))
+        if (Loader::includeModule("iblock") && Loader::includeModule("main"))
         {
-            $oldUserRes = \CUser::GetById($arFields["ID"]);
+            $oldUserRes = CUser::GetById($arFields["ID"]);
             $oldAuthorStatusId = $oldUserRes->Fetch()["UF_AUTHOR_STATUS"];
     
-            $oldAuthorStatusRes = \CIBlockElement::GetByID($oldAuthorStatusId);
+            $oldAuthorStatusRes = CIBlockElement::GetByID($oldAuthorStatusId);
             static::$oldAuthorStatus = $oldAuthorStatusRes->Fetch()["NAME"];
         }
     }
 
     public static function onAfterUserUpdateHandler(&$arFields)
     {
-        if (\Bitrix\Main\Loader::includeModule("iblock") && \Bitrix\Main\Loader::includeModule("main"))
+        if (Loader::includeModule("iblock") && Loader::includeModule("main"))
         {
-            $oldAuthorStatusRes = \CIBlockElement::GetByID($arFields["UF_AUTHOR_STATUS"]);
+            $oldAuthorStatusRes = CIBlockElement::GetByID($arFields["UF_AUTHOR_STATUS"]);
             $newAuthorStatus = $oldAuthorStatusRes->Fetch()["NAME"];
             if (!isset($arFields["RESULT_MESSAGE"]) && static::$oldAuthorStatus !== $newAuthorStatus)
             {
-                \Bitrix\Main\Mail\Event::send([
+                Event::send([
                     "EVENT_NAME" => "EX2_AUTHOR_INFO", 
                     "LID" => "s1", 
                     "C_FIELDS" => array(
